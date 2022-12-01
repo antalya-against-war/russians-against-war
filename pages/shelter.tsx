34f492323b920481eb4b6729a7e7ce6864859baf
Antalya against war <115367775+antalya-against-war@users.noreply.github.com>
2022-12-01 23:20:16 +0300

    text about shelter

--- pages/shelter.tsx
+++ pages/shelter.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Container, PageTitle } from 'styles/commonStyles';
 import {} from 'styles/shelter';
+import { Section, Text } from 'styles/home';
 
 const Shelter: NextPage = () => {
   return (
@@ -12,8 +13,16 @@ const Shelter: NextPage = () => {
       </Head>
 
       <Container>
-        <PageTitle>ШЕЛТЕР</PageTitle>
+        <PageTitle>Шелтер</PageTitle>
+        <Text>
+            Мы принимаем на постоянное жилье украинцев, спасавшихся от войны.
+В пригороде Антальи есть 3 комнатная квартира, где размещаются переселенцы.
+Сейчас там живут 2 семьи - Денис из Херсона (с женой и ребенком), и мама с 2мя детьми из <город>
+Мы закрываем аренду, продукты, одежду, коммуналку для жильцов.
+          Полную историю сборов - читайте в <a href="https://t.me/againstwar_antalya_news">канале Антивоенного сообщества</a>      
+          </Text>
       </Container>
+      
     </>
   )
 };
