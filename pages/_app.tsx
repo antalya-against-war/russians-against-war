a87d0b20c55df0180799bb47fbb98a427d521485
Antalya against war <antalya_against_war@proton.me>
2022-12-13 03:40:00 +0300

    [feature] change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,9 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <Layout navigation={navigation}>
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans"></link>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="icon" href="/favicon.png" />
         </Head>
         <Component {...pageProps} />
