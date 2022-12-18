98a88232cfa4598fbb528890f8ee42e206669c4b
Antalya against war <antalya_against_war@proton.me>
2022-12-18 03:05:25 +0200

    [tech] fix lint issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,10 +11,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <Layout navigation={navigation}>
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="icon" href="/favicon.png" />
         </Head>
         <Component {...pageProps} />
       </Layout>
---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans&display=optional" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
