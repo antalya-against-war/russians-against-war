bb22999a4b0a0367350cfcd2844fb9d484ea8e3b
Antalya against war <antalya_against_war@proton.me>
2022-12-18 03:50:13 +0200

    [tech] remove meta from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato&family=Open+Sans&display=optional" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
